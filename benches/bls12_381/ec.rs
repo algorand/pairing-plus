6b6a3b33639f98241853f9e57b5767d76e789e79
zhenfei <zhenfei.zhang@hotmail.com>
2019-02-06 16:53:59 -0500

    benchmark for membership checks

--- benches/bls12_381/ec.rs
+++ benches/bls12_381/ec.rs
@@ -24,6 +24,24 @@ mod g1 {
     }
 
     #[bench]
+    fn bench_g1_membership(b: &mut ::test::Bencher) {
+        const SAMPLES: usize = 1000;
+
+        let mut rng = XorShiftRng::from_seed([0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
+
+        let v: Vec<G1> = (0..SAMPLES)
+            .map(|_| G1::rand(&mut rng))
+            .collect();
+
+        let mut count = 0;
+        b.iter(|| {
+            let tmp = v[count].into_affine().membership_check();
+            count = (count + 1) % SAMPLES;
+            tmp
+        });
+    }
+
+    #[bench]
     fn bench_g1_mul_assign_sec(b: &mut ::test::Bencher) {
         const SAMPLES: usize = 1000;
 
@@ -197,6 +215,24 @@ mod g2 {
         b.iter(|| {
             let mut tmp = v[count].0;
             tmp.mul_assign(v[count].1);
+            count = (count + 1) % SAMPLES;
+            tmp
+        });
+    }
+
+    #[bench]
+    fn bench_g2_membership(b: &mut ::test::Bencher) {
+        const SAMPLES: usize = 1000;
+
+        let mut rng = XorShiftRng::from_seed([0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
+
+        let v: Vec<G2> = (0..SAMPLES)
+            .map(|_| G2::rand(&mut rng))
+            .collect();
+
+        let mut count = 0;
+        b.iter(|| {
+            let tmp = v[count].into_affine().membership_check();
             count = (count + 1) % SAMPLES;
             tmp
         });
