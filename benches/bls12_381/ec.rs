6515a479214912130135ebadedaba22d2ed18748
zhenfei <zhenfei.zhang@hotmail.com>
2019-04-16 14:46:50 -0400

    Update benches/bls12_381/ec.rs

--- benches/bls12_381/ec.rs
+++ benches/bls12_381/ec.rs
@@ -3,7 +3,7 @@ mod g1 {
     use pairing::CurveAffine;
     use pairing::CurveProjective;
     use rand::{Rand, Rng, SeedableRng, XorShiftRng};
-
+    use ff::PrimeField;
     #[bench]
     fn bench_g1_mul_shamir(b: &mut ::test::Bencher) {
         const SAMPLES: usize = 1000;
@@ -235,6 +235,75 @@ mod g1 {
             tmp
         });
     }
+    #[bench]
+    fn solve_for_y(b: &mut ::test::Bencher) {
+        const SAMPLES: usize = 1000;
+            let mut rng = XorShiftRng::from_seed([0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
+        let mut v: Vec<Fq> = Vec::new();
+        for _i in 0..SAMPLES {
+            let s = Fq::rand(&mut rng);
+            v.push(s);
+        }
+
+        let mut count = 0;
+        b.iter(|| {
+            let tmp = G1Affine::rhs_g1(&v[count]);
+            count = (count + 1) % SAMPLES;
+            tmp
+        });
+    }
+
+    #[bench]
+    fn bench_g1_sw_encode(b: &mut ::test::Bencher) {
+        const SAMPLES: usize = 1000;
+            let mut rng = XorShiftRng::from_seed([0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
+        let mut v: Vec<Fq> = Vec::new();
+        for _i in 0..SAMPLES {
+            let s = Fq::rand(&mut rng);
+            v.push(s);
+        }
+
+        let mut count = 0;
+        b.iter(|| {
+            let tmp = G1Affine::g1_sw_encode(v[count]);
+            count = (count + 1) % SAMPLES;
+            tmp
+        });
+    }
+    #[bench]
+    fn bench_g1_get_point(b: &mut ::test::Bencher) {
+        const SAMPLES: usize = 1000;
+            let mut rng = XorShiftRng::from_seed([0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
+        let mut v: Vec<Fq> = Vec::new();
+        for _i in 0..SAMPLES {
+            let s = Fq::rand(&mut rng);
+            v.push(s);
+        }
+
+        let mut count = 0;
+        b.iter(|| {
+            let tmp = G1Affine::get_point_from_x(v[count], true);
+            count = (count + 1) % SAMPLES;
+            tmp
+        });
+    }
+    #[bench]
+    fn bench_conversion(b: &mut ::test::Bencher) {
+
+
+        pub const SQRT_NEG_THREE: &str = "1586958781458431025242759403266842894121773480562120986020912974854563298150952611241517463240701";
+        pub const SQRT_NEG_THREE_MIN_ONE_DIV_TWO: &str="793479390729215512621379701633421447060886740281060493010456487427281649075476305620758731620350";
+
+        let mut count = 0;
+        b.iter(|| {
+
+                let sqrt_neg_three = Fq::from_str(&SQRT_NEG_THREE).unwrap();
+            //count = (count + 1) % SAMPLES;
+            //tmp
+        });
+    }
+
+
 }
 
 mod g2 {
