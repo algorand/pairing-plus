095c0da576345f1f1b9ea100880643e5f9173605
Riad S. Wahby <kwantam@gmail.com>
2019-10-01 06:49:41 -0700

    hash_to_curve: take AsRef<[u8]> as ciphersuite (#3)

--- src/hash_to_curve.rs
+++ src/hash_to_curve.rs
@@ -11,10 +11,10 @@ type CoordT<PtT> = <PtT as CurveProjective>::Base;
 /// Random oracle and injective maps to curve
 pub trait HashToCurve {
     /// Random oracle
-    fn hash_to_curve<B: AsRef<[u8]>>(msg: B, ciphersuite: u8) -> Self;
+    fn hash_to_curve<B: AsRef<[u8]>, C: AsRef<[u8]>>(msg: B, ciphersuite: C) -> Self;
 
     /// Injective encoding
-    fn encode_to_curve<B: AsRef<[u8]>>(msg: B, ciphersuite: u8) -> Self;
+    fn encode_to_curve<B: AsRef<[u8]>, C: AsRef<[u8]>>(msg: B, ciphersuite: C) -> Self;
 }
 
 impl<PtT> HashToCurve for PtT
@@ -22,9 +22,9 @@ where
     PtT: ClearH + IsogenyMap + OSSWUMap,
     CoordT<PtT>: FromRO,
 {
-    fn hash_to_curve<B: AsRef<[u8]>>(msg: B, ciphersuite: u8) -> PtT {
+    fn hash_to_curve<B: AsRef<[u8]>, C: AsRef<[u8]>>(msg: B, ciphersuite: C) -> PtT {
         let mut p = {
-            let h2f = HashToField::<CoordT<PtT>>::new(msg, Some(&[ciphersuite]));
+            let h2f = HashToField::<CoordT<PtT>>::new(msg, Some(ciphersuite.as_ref()));
             let mut tmp = PtT::osswu_map(&h2f.with_ctr(0));
             tmp.add_assign(&PtT::osswu_map(&h2f.with_ctr(1)));
             tmp
@@ -34,9 +34,9 @@ where
         p
     }
 
-    fn encode_to_curve<B: AsRef<[u8]>>(msg: B, ciphersuite: u8) -> PtT {
+    fn encode_to_curve<B: AsRef<[u8]>, C: AsRef<[u8]>>(msg: B, ciphersuite: C) -> PtT {
         let mut p = {
-            let h2f = HashToField::<CoordT<PtT>>::new(msg, Some(&[ciphersuite]));
+            let h2f = HashToField::<CoordT<PtT>>::new(msg, Some(ciphersuite.as_ref()));
             PtT::osswu_map(&h2f.with_ctr(2))
         };
         p.isogeny_map();
