ed3182bff8d3fc229a399fcb8350a6834f83056b
kwantam <kwantam@gmail.com>
2020-03-12 16:33:11 -0700

    add ExpandMsg type param for HashToCurve

--- src/hash_to_curve.rs
+++ src/hash_to_curve.rs
@@ -9,22 +9,26 @@ use CurveProjective;
 type CoordT<PtT> = <PtT as CurveProjective>::Base;
 
 /// Random oracle and injective maps to curve
-pub trait HashToCurve {
+pub trait HashToCurve<X>
+where
+    X: ExpandMsg
+{
     /// Random oracle
-    fn hash_to_curve<X: ExpandMsg>(msg: &[u8], dst: &[u8]) -> Self;
+    fn hash_to_curve<Mt: AsRef<[u8]>, Dt: AsRef<[u8]>>(msg: Mt, dst: Dt) -> Self;
 
     /// Injective encoding
-    fn encode_to_curve<X: ExpandMsg>(msg: &[u8], dst: &[u8]) -> Self;
+    fn encode_to_curve<Mt: AsRef<[u8]>, Dt: AsRef<[u8]>>(msg: Mt, dst: Dt) -> Self;
 }
 
-impl<PtT> HashToCurve for PtT
+impl<PtT, X> HashToCurve<X> for PtT
 where
     PtT: ClearH + IsogenyMap + OSSWUMap,
     CoordT<PtT>: FromRO,
+    X: ExpandMsg,
 {
-    fn hash_to_curve<X: ExpandMsg>(msg: &[u8], dst: &[u8]) -> PtT {
+    fn hash_to_curve<Mt: AsRef<[u8]>, Dt: AsRef<[u8]>>(msg: Mt, dst: Dt) -> PtT {
         let mut p = {
-            let u = hash_to_field::<CoordT<PtT>, X>(msg, dst, 2);
+            let u = hash_to_field::<CoordT<PtT>, X>(msg.as_ref(), dst.as_ref(), 2);
             let mut tmp = PtT::osswu_map(&u[0]);
             tmp.add_assign(&PtT::osswu_map(&u[1]));
             tmp
@@ -34,9 +38,9 @@ where
         p
     }
 
-    fn encode_to_curve<X: ExpandMsg>(msg: &[u8], dst: &[u8]) -> PtT {
+    fn encode_to_curve<Mt: AsRef<[u8]>, Dt: AsRef<[u8]>>(msg: Mt, dst: Dt) -> PtT {
         let mut p = {
-            let u = hash_to_field::<CoordT<PtT>, X>(msg, dst, 1);
+            let u = hash_to_field::<CoordT<PtT>, X>(msg.as_ref(), dst.as_ref(), 1);
             PtT::osswu_map(&u[0])
         };
         p.isogeny_map();
