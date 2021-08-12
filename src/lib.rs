7a5b826080bd0e8dd21cde150897326e3e8b1c5c
Nikhil Vanjani <nikhilvanjani@ymail.com>
2021-08-12 13:54:07 -0400

    Moved #[allow(...)] to individual items

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,6 @@
 // Force public structures to implement Debug
 #![deny(missing_debug_implementations)]
 
-#![allow(clippy::wrong_self_convention)]
-
 extern crate digest;
 extern crate ff_zeroize as ff;
 extern crate rand_core;
@@ -210,6 +208,7 @@ pub trait CurveProjective:
     fn mul_assign<S: Into<<Self::Scalar as PrimeField>::Repr>>(&mut self, other: S);
 
     /// Converts this element into its affine representation.
+    #[allow(clippy::wrong_self_convention)]
     fn into_affine(&self) -> Self::Affine;
 
     /// Recommends a wNAF window table size given a scalar. Always returns a number
@@ -276,16 +275,19 @@ pub trait CurveAffine:
     fn pairing_with(&self, other: &Self::Pair) -> Self::PairingResult;
 
     /// Converts this element into its affine representation.
+    #[allow(clippy::wrong_self_convention)]
     fn into_projective(&self) -> Self::Projective;
 
     /// Converts this element into its compressed encoding, so long as it's not
     /// the point at infinity.
+    #[allow(clippy::wrong_self_convention)]
     fn into_compressed(&self) -> Self::Compressed {
         <Self::Compressed as EncodedPoint>::from_affine(*self)
     }
 
     /// Converts this element into its uncompressed encoding, so long as it's not
     /// the point at infinity.
+    #[allow(clippy::wrong_self_convention)]
     fn into_uncompressed(&self) -> Self::Uncompressed {
         <Self::Uncompressed as EncodedPoint>::from_affine(*self)
     }
@@ -365,6 +367,7 @@ pub trait EncodedPoint:
 
     /// Converts an `EncodedPoint` into a `CurveAffine` element,
     /// if the encoding represents a valid element.
+    #[allow(clippy::wrong_self_convention)]
     fn into_affine(&self) -> Result<Self::Affine, GroupDecodingError>;
 
     /// Converts an `EncodedPoint` into a `CurveAffine` element,
@@ -374,6 +377,7 @@ pub trait EncodedPoint:
     ///
     /// If the encoding is invalid, this can break API invariants,
     /// so caution is strongly encouraged.
+    #[allow(clippy::wrong_self_convention)]
     fn into_affine_unchecked(&self) -> Result<Self::Affine, GroupDecodingError>;
 
     /// Creates an `EncodedPoint` from an affine point, as long as the
